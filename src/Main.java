4f6c1dc2fe7b951287e601a96771af607b6bda5d
Marcin Nawrot <nawrot_marcin@outlook.com>
2021-04-10 14:29:17 +0200

    rozwiązanie (drugi commit)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uczę");
         System.out.println("się\nprogramowania");
+        System.out.println("i");
+        System.out.println("commitowania");
     }
 }
